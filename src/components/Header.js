e35fe955fce89ebd352cfc414fc6b03681d10de5
Boris <borisavetisyan63@gmail.com>
2022-05-09 16:07:20 +0400

    Home button is changed with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import {HomeIcon} from "../media/SVG";
+import {Logo} from "../media/SVG";
 import {useHistory} from "react-router";
 
 function Header() {
@@ -11,7 +11,7 @@ function Header() {
     return (
         <div className="header-container d-flex align-items-center">
             <div className="home">
-                <span className="home-icon" onClick={onHomeIconClick} >{ HomeIcon }</span>
+                <span className="home-icon" onClick={onHomeIconClick} >{ Logo }</span>
             </div>
             <div className="header-texts">
                 <span className="title-text">Your Secure P@s5w0rd</span>
